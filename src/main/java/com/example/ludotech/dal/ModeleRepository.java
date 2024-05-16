933ee23e66f7f18d8c7ab458fdbaa6ebd76f914a
Evan Guyot <92523408+evan-guyot@users.noreply.github.com>
2024-05-16 09:35:15 +0200

    Feat - Create Modele Class and Its Repository

---
+++ src/main/java/com/example/ludotech/dal/ModeleRepository.java
@@ -0,0 +1,9 @@
+package com.example.ludotech.dal;
+
+import org.springframework.data.jpa.repository.JpaRepository;
+
+import com.example.ludotech.bo.Modele;
+
+public interface ModeleRepository extends JpaRepository<Modele, Integer> {
+
+}
