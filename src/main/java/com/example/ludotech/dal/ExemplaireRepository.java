12a69fc11ae0f611286e574145115229697c777a
Evan Guyot <92523408+evan-guyot@users.noreply.github.com>
2024-05-16 09:11:06 +0200

    Feat - Create Exemplaire and Its Repository

---
+++ src/main/java/com/example/ludotech/dal/ExemplaireRepository.java
@@ -0,0 +1,9 @@
+package com.example.ludotech.dal;
+
+import org.springframework.data.jpa.repository.JpaRepository;
+
+import com.example.ludotech.bo.Exemplaire;
+
+public interface ExemplaireRepository  extends JpaRepository<Exemplaire, Integer>{
+
+}
