952953c50d136797bcadf15f216c2379f4f3dc2f
Evan Guyot <92523408+evan-guyot@users.noreply.github.com>
2024-05-16 10:25:24 +0200

    Feat - Create LocationExamplaire In-Between Class

--- src/main/java/com/example/ludotech/bo/Exemplaire.java
+++ src/main/java/com/example/ludotech/bo/Exemplaire.java
@@ -1,11 +1,15 @@
 package com.example.ludotech.bo;
 
+import java.util.ArrayList;
+import java.util.List;
+
 import jakarta.persistence.Column;
 import jakarta.persistence.Entity;
 import jakarta.persistence.GeneratedValue;
 import jakarta.persistence.Id;
 import jakarta.persistence.JoinColumn;
 import jakarta.persistence.ManyToOne;
+import jakarta.persistence.OneToMany;
 import lombok.AllArgsConstructor;
 import lombok.Data;
 import lombok.EqualsAndHashCode;
@@ -35,4 +39,7 @@ public class Exemplaire {
 	@ManyToOne
 	@JoinColumn(name = "id_reservation", nullable = true)
 	private Reservation reservation;
+	
+	@OneToMany(mappedBy = "exemplaire")
+	private List<LocationExemplaire> locations = new ArrayList<>();
 }
