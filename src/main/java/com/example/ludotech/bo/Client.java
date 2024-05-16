5b808e0bfbe2c8dcda7b062ec1fed180a8126a96
Evan Guyot <92523408+evan-guyot@users.noreply.github.com>
2024-05-16 09:24:07 +0200

    Feat - Create Reservation Class and Its Repository

--- src/main/java/com/example/ludotech/bo/Client.java
+++ src/main/java/com/example/ludotech/bo/Client.java
@@ -1,9 +1,14 @@
 package com.example.ludotech.bo;
 
+import java.util.ArrayList;
+import java.util.List;
+
+import jakarta.persistence.CascadeType;
 import jakarta.persistence.Column;
 import jakarta.persistence.Entity;
 import jakarta.persistence.Inheritance;
 import jakarta.persistence.InheritanceType;
+import jakarta.persistence.OneToMany;
 import jakarta.persistence.OneToOne;
 import lombok.AllArgsConstructor;
 import lombok.Data;
@@ -22,4 +27,8 @@ public class Client extends Utilisateur {
     
     @OneToOne(mappedBy = "client", optional = true)
     private Adresse adresse;
+    
+    @OneToMany(mappedBy = "client", cascade = CascadeType.ALL)
+    private List<Reservation> reservations = new ArrayList<>();
+
 }
